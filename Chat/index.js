408ea5b1c444b07317da71d7841a6d625df92bdb
Obai Alashkar <obai.alashkar@gmail.com>
2022-08-25 16:51:42 +0300

    chat again

--- Chat/index.js
+++ Chat/index.js
@@ -1,12 +1,14 @@
-var messages = document.getElementById("messages");
 function sendMessage() {
   var username = document.getElementById("usernameBox");
   var message = document.getElementById("messageBox");
+  username.value = username.value.trim();
   message.value = message.value.trim();
-  if (message.value != "") {
+
+  if (username.value != "" && message.value != "") {
     document.getElementById("messages").innerHTML +=
       username.value + ": " + message.value + "<br>";
     message.value = "";
   }
+
   message.focus();
 }
