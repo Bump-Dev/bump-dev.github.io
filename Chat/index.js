d3bcefbc1e9c64cde6e4c19ecaa9a818f46d19dc
Obai Alashkar <obai.alashkar@gmail.com>
2022-08-25 17:19:52 +0300

    Made working chat using socket.io

--- Chat/index.js
+++ Chat/index.js
@@ -1,14 +1,20 @@
-function sendMessage() {
-  var username = document.getElementById("usernameBox");
-  var message = document.getElementById("messageBox");
-  username.value = username.value.trim();
-  message.value = message.value.trim();
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-  if (username.value != "" && message.value != "") {
-    document.getElementById("messages").innerHTML +=
-      username.value + ": " + message.value + "<br>";
-    message.value = "";
-  }
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-  message.focus();
-}
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
