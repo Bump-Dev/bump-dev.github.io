33ce075f82532b1625d362667fb9080d58808750
Obai Alashkar <obai.alashkar@gmail.com>
2022-12-02 05:52:05 +0300

    cooler scrolling

---
+++ index.js
@@ -0,0 +1,12 @@
+const sections = document.getElementsByTagName("section");
+var currentSection = 1;
+window.onscroll = function (e) {
+  if (this.scrollY > this.oldScroll && currentSection != sections.length) {
+    currentSection++;
+    document.getElementById(currentSection).scrollIntoView();
+  } else if (this.scrollY < this.oldScroll && currentSection != 1) {
+    currentSection--;
+    document.getElementById(currentSection).scrollIntoView();
+  }
+  this.oldScroll = this.scrollY;
+};
